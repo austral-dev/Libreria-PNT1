96fd65ad89eed1b7ca6b6eaa2487a16ee09009e0
bbonanno03 <92753013+bbonanno03@users.noreply.github.com>
2025-11-03 19:32:01 -0300

    Se agregan archivos .cs en la Carpeta Models

---
+++ Librería-PNT1/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoLibros.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Nombre { get; set; } = string.Empty;
+
+        public ICollection<Libro> Libros { get; set; } = new List<Libro>();
+    }
+}
+

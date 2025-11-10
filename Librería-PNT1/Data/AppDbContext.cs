776eadf8270ddf746142cd67c2152f54b7145b35
bbonanno03 <92753013+bbonanno03@users.noreply.github.com>
2025-11-10 19:50:06 -0300

    cambios en AppDbConectext y Libro

--- Librería-PNT1/Data/AppDbContext.cs
+++ Librería-PNT1/Data/AppDbContext.cs
@@ -15,8 +15,8 @@ namespace Libreria_PNT1.Data
 
             // Seed de prueba
             modelBuilder.Entity<LibroEntity>().HasData(
-                new Libro { IdLibro = "1", Titulo = "El Quijote", Autor = "Cervantes", Descripcion = "Clásico", Precio = 19999.99, Stock = 5, Disponible = true},
-                new Libro { IdLibro = "2", Titulo = "Fundación", Autor = "Isaac Asimov", Descripcion = "Sci-Fi", Precio = 15999.00, Stock = 3, Disponible = true}
+                new LibroEntity { IdLibro = 1, Titulo = "El Quijote", Autor = "Cervantes", Descripcion = "Clásico", Precio = 19999, Stock = 5, Disponible = true},
+                new LibroEntity { IdLibro = 2, Titulo = "Fundación", Autor = "Isaac Asimov", Descripcion = "Sci-Fi", Precio = 15999, Stock = 3, Disponible = true}
             );
         }
     }
--- Librería-PNT1/Models/Libro.cs
+++ Librería-PNT1/Models/Libro.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Libro
 {
-    public string IdLibro { get; set; }
-    public string Titulo { get; set; }
-    public string Autor { get; set; }
-    public string Descripcion { get; set; }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)] // Vos manejás el Id (string "1", "2", etc.)
+    public string IdLibro { get; set; } = string.Empty;
+    public string Titulo { get; set; } = string.Empty;
+    public string Autor { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
     public double Precio { get; set; }
     public int Stock { get; set; }
     public bool Disponible { get; set; }
 
     // Asociación con el enumerado Categoria (Cardinalidad 1)
     public Categoria Categoria { get; set; }
+
+    //Constructor vacío obligatorio para EF
+    public Libro() { }
 
     public Libro(string idLibro, string titulo, string autor, string descripcion, double precio, int stock, Categoria categoria)
     {

cd60b4e08b04b2742cea542b7bffd025cb1c0104
bbonanno03 <92753013+bbonanno03@users.noreply.github.com>
2025-11-23 08:34:53 -0300

    QA a PRD

--- Librería-PNT1/Program.cs
+++ Librería-PNT1/Program.cs
@@ -2,41 +2,57 @@ using Libreria_PNT1.Data;
 using Libreria_PNT1.Repositories;
 using Libreria_PNT1.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// -------------------------
-// Repositorios (inyección de dependencias)
-// -------------------------
-//builder.Services.AddScoped<ILibroRepository, LibroRepository>();
-
-// -------------------------
-// MVC
-// -------------------------
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// -------------------------
-// Middleware HTTP
-// -------------------------
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        // -------------------------
+        // Repositorios (inyección de dependencias)
+        // -------------------------
+        builder.Services.AddScoped<ILibroRepository, LibroRepository>();
+
+
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+        });
+
+        builder.Services.AddHttpContextAccessor();
+
+        // -------------------------
+        // MVC
+        // -------------------------
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // -------------------------
+        // Middleware HTTP
+        // -------------------------
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseSession();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();

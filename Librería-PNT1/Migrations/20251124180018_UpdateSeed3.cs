e4bf9e35f1aba26936b5b1c895da6d8a62a1359a
Ezequiel Reynoso <ezeraf125@gmail.com>
2025-11-24 16:25:26 -0300

    Se agregaron las tapas de los libros en el detalle del libro. Se hicieron ajustes estéticos. Se agregaron créditos en "nosotros".

---
+++ Librería-PNT1/Migrations/20251124180018_UpdateSeed3.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libreria_PNT1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeed3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Libros",
+                keyColumn: "IdLibro",
+                keyValue: 2,
+                column: "Titulo",
+                value: "Fundacion");
+
+            migrationBuilder.UpdateData(
+                table: "Libros",
+                keyColumn: "IdLibro",
+                keyValue: 10,
+                column: "Precio",
+                value: 39999.00m);
+
+            migrationBuilder.InsertData(
+                table: "Libros",
+                columns: new[] { "IdLibro", "Autor", "CategoriaId", "Descripcion", "Disponible", "Imagen", "Precio", "Stock", "Titulo" },
+                values: new object[] { 11, "George Orwell", 1, "Sci-Fi", true, null, 9999.00m, 20, "1984" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Libros",
+                keyColumn: "IdLibro",
+                keyValue: 11);
+
+            migrationBuilder.UpdateData(
+                table: "Libros",
+                keyColumn: "IdLibro",
+                keyValue: 2,
+                column: "Titulo",
+                value: "Fundación");
+
+            migrationBuilder.UpdateData(
+                table: "Libros",
+                keyColumn: "IdLibro",
+                keyValue: 10,
+                column: "Precio",
+                value: 12999.00m);
+        }
+    }
+}
